6f75a9228f7f5be4dcf2c37838c8d0a790e7f78a
Shadman Kolahzary <info@kolahzary.ir>
2016-11-29 17:13:45 +0330

    Color picker known colors

--- ComputerGraphics/ComputerGraphics/ColorPicker.xaml.cs
+++ ComputerGraphics/ComputerGraphics/ColorPicker.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,8 +19,16 @@ namespace ComputerGraphics
     /// <summary>
     /// Interaction logic for ColorPicker.xaml
     /// </summary>
-    public partial class ColorPicker : Window
+    public partial class ColorPicker : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        public void NotifyPropertyChanged_Colors()
+        {
+            this.NotifyPropertyChanged("PickedColor");
+            this.NotifyPropertyChanged("PickedColor_Brush");
+        }
+        public Brush PickedColor_Brush => new SolidColorBrush(this.PickedColor);
         public Color PickedColor
         {
             get
@@ -31,12 +41,14 @@ namespace ComputerGraphics
                 sRed.Value = value.R;
                 sGreen.Value = value.G;
                 sBlue.Value = value.B;
+                this.NotifyPropertyChanged_Colors();
             }
         }
         public ColorPicker()
         {
             InitializeComponent();
         }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             this.PickedColor = Colors.Black;
@@ -44,7 +56,7 @@ namespace ComputerGraphics
 
         private void Sliders_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            cColorPreview.Background = new SolidColorBrush(this.PickedColor);
+            this.NotifyPropertyChanged_Colors();
         }
 
         private void btnOk_Click(object sender, RoutedEventArgs e)
@@ -59,5 +71,9 @@ namespace ComputerGraphics
             this.Close();
         }
 
+        private void cbKnownColors_Selected(object sender, RoutedEventArgs e)
+        {
+            this.PickedColor = ((Color)ColorConverter.ConvertFromString((string)cbKnownColors.SelectedValue));
+        }
     }
 }
